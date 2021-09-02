9229da1c4219cc7ff9745e10b6563d54ffb8ac69
rafifirmansyah <rafi@unzypsoft.com>
2021-09-02 16:41:11 +0700

    update

--- pages/users/[id].tsx
+++ pages/users/[id].tsx
@@ -1,17 +1,44 @@
 import { useRouter } from 'next/router';
+import Layout from '../../components/Layout';
 
-export default function Detail() {
-    const router = useRouter();
-    const { id } = router.query;
+interface UserDetailProps {
+    user: object;
+}
+
+export default function UserDetail(props: UserDetailProps) {
+    const { user } = props;
 
     return (
-        <div>
-            <p>
-                User Detail Page
-                {' '}
-                {id}
-                {' '}
-            </p>
-        </div>
+        <Layout pageTitle="User Detail">
+            <p>{user.name}</p>
+            <p>{user.email}</p>
+        </Layout>
     );
+}
+
+export async function getStaticPaths() {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const dataUsers = await res.json();
+
+    const paths = dataUsers.map((user) => ({
+        params: {
+            id: `${user}`,
+        },
+    }));
+
+    return {
+        paths,
+    };
+}
+
+export async function getStaticProps(context) {
+    const { id } = context.params;
+    const res = await fetch(`https://jsonplaceholder.typicode.com/users${id}`);
+    const user = await res.json();
+
+    return {
+        props: {
+            user,
+        },
+    };
 }
--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -1,34 +1,36 @@
+import { useRouter } from 'next/router';
 import Layout from '../../components/Layout';
+import style from '../../styles/Users.module.css';
 
 interface UsersProps {
     dataUsers: Array<any>;
 }
 
 export default function Users(props: UsersProps) {
-  const { dataUsers } = props;
-  console.log(dataUsers);
+    const { dataUsers } = props;
+    const router = useRouter();
 
-  return (
-    <Layout pageTitle="Users Page">
-      {
-        dataUsers.map((user) => (
-          <div key={user.id}>
-            <p>{user.name}</p>
-            <p>{user.email}</p>
-          </div>
-        ))
-      }
-    </Layout>
-  );
+    return (
+        <Layout pageTitle="Users Page">
+        {
+            dataUsers.map((user) => (
+                <div key={user.id} onClick={() => router.push(`/users/${user.id}`)} className={style.card}>
+                    <p>{user.name}</p>
+                    <p>{user.email}</p>
+                </div>
+            ))
+        }
+        </Layout>
+    );
 }
 
 export async function getStaticProps() {
-  const res = await fetch('https://jsonplaceholder.typicode.com/users');
-  const dataUsers = await res.json();
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const dataUsers = await res.json();
 
-  return {
-    props: {
-      dataUsers,
-    },
-  };
+    return {
+        props: {
+        dataUsers,
+        },
+    };
 }
